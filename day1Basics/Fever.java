7d74d16a7deba5927a8039dcd0836e98ca94470f
vanshika898 <vanshikanigam8989@gmail.com>
2024-03-08 21:01:45 +0530

    q2

---
+++ day1Basics/Fever.java
@@ -0,0 +1,13 @@
+
+class Fever {
+    public static void main(String gg[]) {
+        double temp = 101.5;
+        if (temp > 100) {
+            System.out.println("you have fever !");
+
+        } else {
+            System.out.println("You have no fever");
+        }
+    }
+
+}
