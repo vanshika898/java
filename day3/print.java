97f3dd57b9c1b7e1663c902964befa90043f6779
vanshika898 <vanshikanigam8989@gmail.com>
2024-03-10 22:52:55 +0530

    printnumber1 to10

---
+++ day3/print.java
@@ -0,0 +1,11 @@
+package day3;
+
+class print {
+    public static void main(String[] args) {
+        int i = 1;
+        while (i <= 10) {
+            System.out.println(i);
+            i++;
+        }
+    }
+}
