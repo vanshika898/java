839315b6cbadb33194077ee8ce70ef870532d907
vanshika898 <vanshikanigam8989@gmail.com>
2024-03-10 23:02:34 +0530

    pyramid

---
+++ day3/pyramid.java
@@ -0,0 +1,14 @@
+package day3;
+
+public class pyramid {
+    public static void main(String[] args) {
+        int N = 5;
+        for (int i = 0; i <= N; i++) {
+            for (int j = 0; j < i; j++) {
+                System.out.print('*');
+            }
+            System.out.println();
+        }
+    }
+
+}
